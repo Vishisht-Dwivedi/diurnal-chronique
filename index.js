22a639c8f11ae8bffe1d0bdb8381bd4c5671df38
Vishisht-Dwivedi <vishisht22868@gmail.com>
2024-07-23 22:05:38 +0530

    reconfigured the API handling and built up object constructors to deal with dynamic updating

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const axios = require('axios');
 const path = require('path');
 const app = express();
-const topNewsModel = require('./models/topNews');
+const topNewsModel = require(path.join(__dirname, '/models/topNews'));
 //using and setting paths
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, '/views'));
@@ -18,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/News_Website')
     .catch((err) => {
         console.log("ERROR", err);
     });
+
 //date
 let currentDate = new Date();
 const options = {
@@ -30,12 +31,10 @@ let parsedDate;
 let data;
 //date Parsing
 function dateConstructor(dateline) {
-
     const dateArr = dateline.split(',').join('').split(' ');
     let dateObject = new Date(`${dateArr[0]} ${dateArr[1]}, ${dateArr[2]}`)
     let time;
     let day;
-
     //time formatting
     const unformattedTime = dateArr[3];
     if (unformattedTime[5] === "P" && Number(unformattedTime.slice(0, 3)) < 12) {
@@ -49,7 +48,6 @@ function dateConstructor(dateline) {
     } else {
         time = unformattedTime.slice(0, 5);
     }
-
     //day formatting
     let fday = Number(dateArr[1]);
     if (String(fday).length !== 2) {
@@ -57,56 +55,61 @@ function dateConstructor(dateline) {
     } else {
         day = fday;
     }
-
     //month formatting
     let fmonth = dateObject.getMonth() + 1;
     let month;
-
     if (String(fmonth).length !== 2) {
         month = '0' + fmonth;
     } else {
         month = fmonth;
     }
-
     let year = dateObject.getFullYear();
-
     return `${year}${month}${day}${time}`;
 }
-//routes
-app.use(async (req, res, next) => {
-
-    axios.get('https://timesofindia.indiatimes.com/feeds/newsdefaultfeeds.cms?feedtype=sjson#')
+//MONGOOSE MODEL HANDLING
+//object construction that takes an element of apidata as an argument
+class TopHeadlinesObj {
+    constructor(element) {
+        this.Headline = element.HeadLine;
+        this.ByLine = element.ByLine;
+        this.Date = element.DateLine;
+        this.Photo = element.Image.Photo;
+        this.Caption = element.Caption;
+        this.Keywords = element.Keywords.split(',');
+        this.Story = element.Story;
+        this.WebURL = element.WebURL;
+        this.DateNumber = dateConstructor(element.DateLine)
+    }
+}
+//updating function that takes in the model to put data in, the constructor objects and the element which needs to be passed into the constructor functions.
+async function updateToDB(Model, Object, element) {
+    const options = { upsert: true, new: true }
+    const obj = new Object(element);
+    Model.findOneAndUpdate({ _id: Number(element.NewsItemId) }, obj, options)
+        .then(() => { return })
+        .catch((err) => console.log('Error in updating elements->', err));
+}
+//API URLS
+const topHeadlinesAPI = "https://timesofindia.indiatimes.com/feeds/newsdefaultfeeds.cms?feedtype=sjson#";
+//API HANDLING
+async function getDataAndUpdate(model, Object, apiURL) {
+    return axios.get(apiURL)
         .then((response) => {
-
-
             const newsArr = response.data.NewsItem;
             for (let element of newsArr) {
-                const options = { upsert: true, new: true }
-                topNewsModel.findOneAndUpdate({ _id: Number(element.NewsItemId) }, {
-                    Headline: element.HeadLine,
-                    ByLine: element.ByLine,
-                    Date: element.DateLine,
-                    Photo: element.Image.Photo,
-                    Caption: element.Caption,
-                    Keywords: element.Keywords.split(','),
-                    Story: element.Story,
-                    WebURL: element.WebURL,
-                    DateNumber: dateConstructor(element.DateLine)
-                }, options)
-                    .then(() => { return })
-                    .catch((err) => console.log('Error in updating elements->', err));
+                updateToDB(model, Object, element);
             }
-            next();
         })
         .catch((err) => {
             console.log("Error in connection with API->", err);
-            next();
         });
+}
 
-    data = await topNewsModel.find({}).sort({ DateNumber: -1 }).limit(26);
 
-})
+//routes
 app.get('/', async (req, res) => {
+    getDataAndUpdate(topNewsModel, TopHeadlinesObj, topHeadlinesAPI);
+    data = await topNewsModel.find({}).sort({ DateNumber: -1 }).limit(26);
     parsedDate = currentDate.toLocaleDateString('us-EN', options);
     res.render('index', { date: parsedDate, data: data });
 })
