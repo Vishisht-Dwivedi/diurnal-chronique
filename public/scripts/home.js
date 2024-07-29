ebef79b5b7908a3798e33a1aea06c7e1752452e7
Vishisht-Dwivedi <vishisht22868@gmail.com>
2024-07-29 16:16:37 +0530

    finished up the scrolling button functionality

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -3,7 +3,7 @@ console.log('connected');
 let fontChangeTxt = document.querySelector('#hlfontchange').innerText;
 const changedText = [];
 for (let e of fontChangeTxt) {
-    const arr = `'!@#$%^&*()_,-[]:.?"`;
+    const arr = `'!@#$%^&*()_,-[]:.?;"`;
     if (arr.indexOf(e) !== -1) {
         changedText.push(`<span style='font-family:oldnewspaper;'>${e}</span>`);
     } else {
@@ -11,3 +11,36 @@ for (let e of fontChangeTxt) {
     }
 };
 document.querySelector('#hlfontchange').innerHTML = changedText.join('');
+
+//scroll buttons
+const scrollDown = document.querySelectorAll('.sd');
+scrollDown.forEach(element => {
+    element.addEventListener('click', () => {
+        element.parentElement.scrollBy(0, 200);
+    })
+});
+const scrollUp = document.querySelectorAll('.su');
+scrollUp.forEach(element => {
+    element.addEventListener('click', () => {
+        element.parentElement.scrollBy(0, -200);
+    })
+});
+const heroLeft = document.querySelectorAll('.scrollable');
+
+heroLeft.forEach(element => {
+    element.addEventListener('scroll', () => {
+        const su = element.querySelector('.su');
+        const sd = element.querySelector('.sd');
+        const div = element;
+        if (div.scrollTop > 50) {
+            su.style.display = 'block';
+            if ((div.scrollTop - (div.scrollHeight - div.offsetHeight)) > 0) {
+                sd.style.display = 'none';
+            } else {
+                sd.style.display = 'block';
+            }
+        } else if (div.scrollTop === 0) {
+            su.style.display = 'none';
+        }
+    })
+}); 
